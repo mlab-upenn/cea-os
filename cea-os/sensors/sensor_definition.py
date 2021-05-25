1479b5d5dace149144835d3c0e1d01046326a90c
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-25 17:06:16 -0400

    Update sensor_definition.py

--- cea-os/sensors/sensor_definition.py
+++ cea-os/sensors/sensor_definition.py
@@ -4,19 +4,29 @@ This file contains the basic interface for sensors in this code base.
 import random
 
 class Sensor:
-    def __init__(self, value = 0, constant = 1) -> None:
+    def __init__(self, value = 0, noise = 0) -> None:
         self.value = value
-        self.constant = constant
+        self.noise = noise
     
     def read_value(self):
         """
         This method returns the value of the sensor
         """
-        if self.constant == 0:
-            self.value += random.randint(-5,5)
-        
+        self.value += random.gauss(0, noise)
         return self.value
     
+    def set_value(self, value):
+        try:
+            self.value = (float) value
+        except ValueError:
+            print("INVALID VALUE")
+    
+    def set_noise(self, noise):
+        try:
+            self.noise = (float) noise
+        except ValueError:
+            print("INVALID VALUE")
+            
     def calibrate(self):
         """
         This method is used to calibrate the sensor. Need not be implemented for sensors that don't require calibration
