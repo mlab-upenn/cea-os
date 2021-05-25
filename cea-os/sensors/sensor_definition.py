485c10e6a9efb11404955a727d7fe32ad74b1bbd
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-25 16:50:44 -0400

    Update sensor_definition.py

--- cea-os/sensors/sensor_definition.py
+++ cea-os/sensors/sensor_definition.py
@@ -1,16 +1,21 @@
 """
 This file contains the basic interface for sensors in this code base.
 """
+import random
 
 class Sensor:
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, value = 0, constant = 1) -> None:
+        self.value = value
+        self.constant = constant
     
     def read_value(self):
         """
         This method returns the value of the sensor
         """
-        raise NotImplementedError
+        if self.constant == 0:
+            self.value += random.randint(-5,5)
+        
+        return self.value
     
     def calibrate(self):
         """
