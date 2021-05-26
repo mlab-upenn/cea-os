393fba7c7eb85a27fd8aa998d47153d5ca06f429
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-26 09:56:30 -0400

    Update sensor_definition.py

--- cea-os/sensors/sensor_definition.py
+++ cea-os/sensors/sensor_definition.py
@@ -5,35 +5,13 @@ import random
 
 class Sensor:
     def __init__(self, value = 0, noise = 0) -> None:
-        try:
-            self.value = float(value)   #sets initial value for sensor data
-        except ValueError:
-            self.value = 0
-            print("INVALID VALUE")          
-        try:
-            self.noise = float(noise)   #sets level of noise (noise = 0 gives constant value)
-        except ValueError:
-            self.noise = 0
-            print("INVALID NOISE")
+        pass
     
     def read_value(self):
         """
         This method returns the value of the sensor
         """
-        self.value += random.gauss(0, noise)    #adds/subtracts random val from gauss. dist.
-        return self.value
-    
-    def set_value(self, value):
-        try:
-            self.value = float(value)
-        except ValueError:
-            print("INVALID VALUE")
-    
-    def set_noise(self, noise):
-        try:
-            self.noise = abs(float(noise))
-        except ValueError:
-            print("INVALID VALUE")
+        pass
             
     def calibrate(self):
         """
