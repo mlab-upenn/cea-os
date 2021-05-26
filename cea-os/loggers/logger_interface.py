4eabe11fc72dce8f016f389f0a1c5e9b10e1e028
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-26 17:05:55 -0400

    Update logger_interface.py

--- cea-os/loggers/logger_interface.py
+++ cea-os/loggers/logger_interface.py
@@ -1,1 +1,13 @@
-def class Logger(self):
+class Logger():
+  def __init__(self) -> None:
+      raise NotImplementedError
+      
+  def configure(self):
+      raise NotImplementedError
+      
+  def log_data(self):
+      raise NotImplementedError
+      
+  def add_sensor(self):
+      raise NotImplementedError
+    
