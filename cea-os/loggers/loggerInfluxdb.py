d0da8dd455d8e0b89cdc6832c1714edd2067fc01
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-27 16:45:36 -0400

    Update loggerInfluxdb.py

--- cea-os/loggers/loggerInfluxdb.py
+++ cea-os/loggers/loggerInfluxdb.py
@@ -1,1 +1,24 @@
-class loggerInfluxDB:
+from influxdb_client import InfluxDBClient
+from ..sensors import sensors
+
+class LoggerInfluxDB():
+  def __init__(self) -> None:
+    self.client = None
+    self.write_api = None
+    self.sensor = None
+    self.pollrate = 10  #seconds between data logs
+    
+  def configure(self, url, token, org):
+    self.client = InfluxDBClient(url = url, token = token, org = org)
+    self.write_api = self.client.write_api()
+    
+  def send_logs(self):
+    pass
+  
+  def set_sensor(self, Sensor):
+    self.sensor = Sensor
+    
+  def set_pollrate(self, pollrate):
+    self.pollrate = pollrate
+    
+    
