7f2dc93b16496329bd673ec6c7d97c71fecdc5db
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-27 10:33:05 -0400

    Update logger_interface.py

--- cea-os/loggers/logger_interface.py
+++ cea-os/loggers/logger_interface.py
@@ -2,15 +2,15 @@ class Logger():
   def __init__(self) -> None:
       raise NotImplementedError
       
-  def configure(self):
+  def configure(self):  #configure connection to database and set up table for data
       raise NotImplementedError
       
-  def send_logs(self):
+  def send_logs(self):  #send logs to database
       raise NotImplementedError
       
-  def set_bed(self, bed):
+  def set_bed(self, bed): #set the bed that is being logged
       raise NotImplementedError
       
-  def set_pollrate(self):
+  def set_pollrate(self): #set the pollrate for the sensors in the bed
       pass
     
