828499b5d085411444901c76467d84f182a6721f
Aditya Gupta <adigupta@seas.upenn.edu>
2021-05-27 14:32:29 -0700

    Skeleton

--- cea-os/objects/environment.py
+++ cea-os/objects/environment.py
@@ -3,23 +3,25 @@ This file contains an environment model
 """
 
 from beds import Bed
-#from ..sensor_definition import Sensor
-#Unable to get the sensor import to work
+from ..sensors.sensor_definition import Sensor
 
 class Environment:
-	def __init__(self, name, farm, temperature = 0, water = 0, nutrients = 0, lighting = 'LED', light_intensity = 1):
-		self.temperature = temperature
-		self.water = water
-		self.nutrients = nutrients
+	#temperature, nutrients, water, lighting, light intensity will be added later
+	def __init__(self, name, farm):
+		#self.temperature = temperature
+		#self.water = water
+		#self.nutrients = nutrients
 		self.name = name
 		# I use a dictionary because we do not want duplicates names
 		self.beds = dict() #keeps track of beds (name is key)
 		self.num_beds = 0
 		self.farm = farm #farm name environment is associated with
-		self.lighting = lighting
+		#self.lighting = lighting
 		self.sensors = list() # Some sensors will be on an environment level
 		self.num_sensors = 0
-		self.light_intensity = light_intensity #measured in lumens per square meter?
+		self.actuators = list()
+		self.num_actuators = list()
+		#self.light_intensity = light_intensity #measured in lumens per square meter?
 
 	def add_bed(self, bed):
 		self.beds[bed.name] = bed
@@ -47,6 +49,11 @@ class Environment:
 		toadd = (name, new_sensor)
 		self.sensors.append(toadd)
 		self.num_sensors += 1 
+
+	def add_actuators(self, name, actuator):
+		toadd = (name, actuator)
+		self.actuators.append(toadd)
+		self.num_actuators += 1
 
 	def current_temperature(self, sensor):
 		#poll environment level sensor to see if it is within accetable range
