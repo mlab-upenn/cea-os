2c7f166b57a3cff5698c830b33545a727c6e6cf2
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-28 15:05:34 -0400

    Update logger_interface.py

--- cea-os/loggers/logger_interface.py
+++ cea-os/loggers/logger_interface.py
@@ -10,7 +10,4 @@ class Logger():
       
   def set_sensor(self, Sensor): #set the sensor that is being logged
       raise NotImplementedError
-      
-  def set_pollrate(self): #set the pollrate for the sensors in the bed
-      pass
     
