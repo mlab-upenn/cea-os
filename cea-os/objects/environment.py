68f7c334186c07a0beadd3402375eecdc879a4cc
Aditya Gupta <adigupta@seas.upenn.edu>
2021-05-27 19:04:02 -0700

    good skeleton

--- cea-os/objects/environment.py
+++ cea-os/objects/environment.py
@@ -7,21 +7,15 @@ from ..sensors.sensor_definition import Sensor
 
 class Environment:
 	#temperature, nutrients, water, lighting, light intensity will be added later
-	def __init__(self, name, farm):
-		#self.temperature = temperature
-		#self.water = water
-		#self.nutrients = nutrients
-		self.name = name
+	def __init__(self):
 		# I use a dictionary because we do not want duplicates names
 		self.beds = dict() #keeps track of beds (name is key)
 		self.num_beds = 0
-		self.farm = farm #farm name environment is associated with
-		#self.lighting = lighting
 		self.sensors = list() # Some sensors will be on an environment level
 		self.num_sensors = 0
 		self.actuators = list()
 		self.num_actuators = list()
-		#self.light_intensity = light_intensity #measured in lumens per square meter?
+		self.properties = dict()
 
 	def add_bed(self, bed):
 		self.beds[bed.name] = bed
@@ -29,21 +23,6 @@ class Environment:
 
 	def delete_bed(self, bed):
 		del self.beds[bed.name]
-
-	def set_temperature(self, temperature):
-		self.temperature = temperature
-
-	def set_water(self, water):
-		self.water = water
-
-	def set_nutrients(self, nutrients):
-		self.nutrients = nutrients
-
-	def set_name(self, name):
-		self.name = name
-
-	def set_lighting(self, lighting):
-		self.lighting = lighting
 
 	def add_sensor(self, name, new_sensor):
 		toadd = (name, new_sensor)
@@ -55,25 +34,11 @@ class Environment:
 		self.actuators.append(toadd)
 		self.num_actuators += 1
 
-	def current_temperature(self, sensor):
-		#poll environment level sensor to see if it is within accetable range
-		#have some kind of logic that will actuate the system or raise an alarm if not within range
-		raise NotImplementedError
+	def add_property(self, key, value):
+		self.properties[key] = value
 
-	def current_water(self, sensor):
-		#poll environment level sensor to see if it is within accetable range
-		#have some kind of logic that will actuate the system or raise an alarm if not within range
-		raise NotImplementedError
-
-	def current_nutrient(self, sensor):
-		#poll environment level sensor to see if it is within accetable range
-		#have some kind of logic that will actuate the system or raise an alarm if not within range
-		raise NotImplementedError
-
-	def current_light_intensity(self, sensor):
-		#poll environment level sensor to see if it is within accetable range
-		#have some kind of logic that will actuate the system or raise an alarm if not within range
-		raise NotImplementedError
+	def delete_property(self, key):
+		del self.properties[key]
 
 
 
