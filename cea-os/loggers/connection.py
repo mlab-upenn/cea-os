88dd91c38eff084a46e33bc2d93cabb3e8b1ac03
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-31 19:37:56 -0400

    Update connection.py

--- cea-os/loggers/connection.py
+++ cea-os/loggers/connection.py
@@ -2,9 +2,9 @@ class DBConnection():
   def __init__(self) -> None:
     raise NotImplementedError
   
-  def configure(self):
+  def configure(self):  #establish connection to database
     raise NotImplementedError
     
-  def return_connection(self):
+  def return_connection(self):  #return the database client created by connection
     raise NotImplementedError
   
