dc570b645156f6a91702062f31754d6f72014b42
kxu112 <84808479+kxu112@users.noreply.github.com>
2021-05-31 19:32:22 -0400

    Update logger_interface.py

--- cea-os/loggers/logger_interface.py
+++ cea-os/loggers/logger_interface.py
@@ -2,16 +2,10 @@ class Logger():
   def __init__(self) -> None:
       raise NotImplementedError
       
-  def connect(self):  #establish connection to db
-      raise NotImplementedError
-      
-  def create_table(self): #create db table
-      raise NotImplementedError
-      
   def set_refresh_rate(self): #set refresh rate of data logging
       raise NotImplementedError
       
-  def send_logs(self):  #send logs to database
+  def send_logs(self):  #send logs to database using client established by Connection object
       raise NotImplementedError
       
   def set_sensor(self, Sensor): #set a sensor that is being logged
