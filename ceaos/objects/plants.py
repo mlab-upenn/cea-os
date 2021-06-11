a2cdfdf7952fd9317598e16ed6caa3e8da84cce6
Nandan Tumu <nandan.t@live.com>
2021-06-11 12:42:39 -0400

    Autoformatted and removed unused imports.

--- ceaos/objects/plants.py
+++ ceaos/objects/plants.py
@@ -2,17 +2,18 @@
 This file contains an plant model
 """
 
+
 class Plant:
-	def __init__(self, name):
-		# general properties dictionary
-		self.name = name
-		self.properties = dict()
+    def __init__(self, name):
+        # general properties dictionary
+        self.name = name
+        self.properties = dict()
 
-	def set_name(self, name):
-		self.name = name
+    def set_name(self, name):
+        self.name = name
 
-	def add_property(self, key, value):
-		self.properties[key] = value
+    def add_property(self, key, value):
+        self.properties[key] = value
 
-	def delete_property(self, key):
-		del self.properties[key]
+    def delete_property(self, key):
+        del self.properties[key]
