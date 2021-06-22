eb96cb6ac4f397f0d45c5b82d68a62f70fcdebf9
Sarah Santos <sarahmsant@gmail.com>
2021-06-22 01:29:44 -0400

    Updated set_value() to pull from DHT22

--- ceaos/sensors/temperature_sensor.py
+++ ceaos/sensors/temperature_sensor.py
@@ -1,17 +1,29 @@
 """
 This file contains a class for a temperature sensor
 """
-from .sensor_definition import Sensor  
-from ceaos.loggers.InfluxDB import InfluxDBConnection, InfluxDBLogger
+import os
+import sys
+os.chdir("pigpio_dht22")
+sys.path.append("/home/pi/pigpio_dht22")
+import pigpio
+import DHT22
+from sensor_definition import Sensor
 
 
 class Temperature(Sensor):
-
-    datatype = "General_Temp"  # static variable for Temperature class
-
-    def __init__(self, temp_value=None, datatype=datatype) -> None:
+    def __init__(self, gpio=4, temp_value=None, datatype="General_Temp") -> None:
+        self.set_datatype(datatype)
+        self.pi = pigpio.pi()
+        self.set_gpio(gpio)
+        self.sensor = DHT22.sensor(self.pi, self.gpio)
         self.set_value(temp_value)
-        self.set_datatype(datatype) # defaults to "General_Temp" (i.e. instead of Air_Temp)
+
+    def set_gpio(self, gpio: int):
+        try:
+            self.gpio = int(gpio)
+        except ValueError:
+            self.gpio = None
+            raise ValueError("INVALID VALUE")
 
     def set_value(self, temp_value: float):
         """
@@ -24,10 +36,25 @@ class Temperature(Sensor):
                 self.value = None
                 raise ValueError("INVALID VALUE")
         else:
-            self.value = None
-   
+            self.sensor.trigger()
+            self.value = self.get_temp(self.sensor)
+            #### ISSUE: gets caught in an infinite loop
+            # while (self.value == -999.0):
+            #   self.sensor.trigger()
+            #  self.value = self.get_temp(self.sensor)
+
     def set_datatype(self, datatype: str):
         self.datatype = datatype
+
+    def get_temp(
+        self, phys_sensor: DHT22
+    ):  # what datatype is phys_sensor? what does DHT22.sensor() return?
+        if phys_sensor.temperature() != None:
+            newTemp = phys_sensor.temperature()
+        else:
+            newTemp = None
+            print("Error: sensor read failed")
+        return newTemp
 
     def read_value(self):
         """
@@ -35,5 +62,8 @@ class Temperature(Sensor):
         """
         return self.value
 
-    def get_datatype(self):  # returns the measurement the sensor is recording (i.e. temperature, pH)
+    def get_datatype(self):
+        """
+        This method returns the measurement the sensor is recording (i.e. temperature, pH)
+        """
         return self.datatype
