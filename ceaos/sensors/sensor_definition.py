c291becb8237a4e9a5be43d8dfbb72d78c29d228
kxu11235 <xukev@seas.upenn.edu>
2021-07-02 13:37:38 -0400

    got rid of calibrate method

--- ceaos/sensors/sensor_definition.py
+++ ceaos/sensors/sensor_definition.py
@@ -12,9 +12,3 @@ class Sensor:
         This method returns the value of the sensor
         """
         raise NotImplementedError
-
-    def calibrate(self):
-        """
-        This method is used to calibrate the sensor. Need not be implemented for sensors that don't require calibration
-        """
-        pass
