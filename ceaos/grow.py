c94476181be357ee0ada95aeffabefe59525eec6
Aditya Gupta <adigupta@seas.upenn.edu>
2021-07-31 10:41:28 -0700

    "parse grow recipes"

--- ceaos/grow.py
+++ ceaos/grow.py
@@ -1,19 +1,13 @@
 from ceaos.actuators.actuator_commands import set_airtemp
 import yaml
-import zmq
 from datetime import datetime, time
 from .objects.farm import Farm
 from .objects.beds import Bed
 from .objects.environment import Environment
 from .actuators import actuator_commands
 from importlib_resources import files
-import logging
-import os
-
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
 def get_allbeds(farm):
-    logging.info("I was here1")
     all_beds = dict()
     environments = farm.get_envs()
     for environment in environments:
@@ -24,7 +18,6 @@ def get_allbeds(farm):
     return all_beds
 
 def find_relevantbeds(bed_names_list, all_beds):
-    logging.info("I was here2")
     bed_list = []
     for names in bed_names_list:
         for names2 in all_beds:
@@ -34,7 +27,6 @@ def find_relevantbeds(bed_names_list, all_beds):
     return bed_list
 
 def find_ips(bed_list, actuator):
-    logging.info("I was here3")
     ip_list = []
     for bed in bed_list:
         for act in bed.get_actuators():
@@ -44,7 +36,6 @@ def find_ips(bed_list, actuator):
     return ip_list
 
 def send_command(ip_list, max, min, actuation):
-    logging.info("I was here4")
     for ip in ip_list:
         if actuation == "air_temperature":
             actuator_commands.set_airtemp(ip, max, min)
@@ -57,11 +48,45 @@ def send_command(ip_list, max, min, actuation):
         elif actuation == "pH":
             actuator_commands.set_pH(ip, max, min)
         elif actuation == "EC":
-            actuator_commands.set_EC(ip, max, min)    
+            actuator_commands.set_EC(ip, max, min)
+
+def day_or_night(recipe):
+    #this time stuff is not accurate, more of a placeholder
+    for timeperiod in recipe["air_temperature"]:
+        if datetime.utcnow().time() < time(20, 0) and datetime.utcnow().time() > time(6, 0):
+            period = timeperiod
+        else:
+            period = timeperiod
+    
+    return period
+
+def parse_recipe(recipe, bed_list):
+    for k in recipe:
+        if k == 'air_temperature':
+            period = day_or_night(recipe)
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, period["max"], period["min"], "air_temperature")
+        elif k == 'water_temperature':
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, recipe[k]['max'], recipe[k]['min'], "water_temperature")
+        elif k == 'relative_humidity':
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, recipe[k]['max'], recipe[k]['min'], "relative humidity")
+        elif k == 'light_hours':
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, recipe[k]['max'], recipe[k]['min'], "light_hours")
+        elif k == 'DLI':
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, recipe[k]['max'], recipe[k]['min'], "DLI")
+        elif k == 'pH':
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, recipe[k]['max'], recipe[k]['min'], "pH")
+        elif k == 'EC':
+            ip_list = find_ips(bed_list, k)
+            send_command(ip_list, recipe[k]['max'], recipe[k]['min'], "EC")
 
 
 def load_grow(farm, config_folder="ceaos.resources.config", config_file="config_lettuce_grow.yml"):
-    logging.info("I was here")
     config = files(config_folder).joinpath(config_file).read_text()
     try:
         dictionary = yaml.safe_load(config)
@@ -99,45 +124,9 @@ def load_grow(farm, config_folder="ceaos.resources.config", config_file="config_
             recipe3 = stages
             recipe_list.append(recipe3)
         
-    for k in recipe1:
-        if k == 'air_temperature':
-            #this time stuff is not accurate, more of a placeholder
-            for timeperiod in recipe1["air_temperature"]:
-                #if datetime.utcnow().time() < time(20, 0) and datetime.utcnow().time() > time(6, 0):
-                if timeperiod["time_period"] == "day":
-                    period = timeperiod
-                else:
-                    period = timeperiod
-
-            logging.info("here")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, period["max"], period["min"], "air_temperature")
-        elif k == 'water_temperature':
-            logging.info("here1")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, recipe1[k]['max'], recipe1[k]['min'], "water_temperature")
-        elif k == 'relative_humidity':
-            logging.info("here2")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, recipe1[k]['max'], recipe1[k]['min'], "relative humidity")
-        elif k == 'light_hours':
-            logging.info("here3")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, recipe1[k]['max'], recipe1[k]['min'], "light_hours")
-        elif k == 'DLI':
-            logging.info("here4")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, recipe1[k]['max'], recipe1[k]['min'], "DLI")
-        elif k == 'pH':
-            logging.info("here5")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, recipe1[k]['max'], recipe1[k]['min'], "pH")
-        elif k == 'EC':
-            logging.info("here6")
-            ip_list = find_ips(bed_list, k)
-            send_command(ip_list, recipe1[k]['max'], recipe1[k]['min'], "EC")
-        else:
-            print("do nothing")
+    for recipe in recipe_list:
+        if recipe == recipe1:
+            parse_recipe(recipe, bed_list)
         
     return recipe_list
         
