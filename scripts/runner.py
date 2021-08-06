03f5f272838276e88ad00fe19cd0323d446181f9
Aditya Gupta <adigupta@seas.upenn.edu>
2021-08-05 22:19:05 -0400

    "fix file path"

--- scripts/runner.py
+++ scripts/runner.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     recipe_list = []
     c = os.getcwd()
     print(c)
-    directory = "cea-os.ceaos.resources.config.recipes"
+    directory = "/ceaos/resources/config/recipes"
 
     for recipe in os.scandir(directory):
         recipe_to_add = load_grow(directory, recipe)
