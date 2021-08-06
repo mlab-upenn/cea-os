af8e971e0eabab271774e1602ed5c8f5bd2009c2
Aditya Gupta <adigupta@seas.upenn.edu>
2021-08-05 22:37:47 -0400

    "need to fix file path"

--- scripts/runner.py
+++ scripts/runner.py
@@ -54,8 +54,6 @@ if __name__ == "__main__":
 
     # this is a list of lists
     recipe_list = []
-    c = os.getcwd()
-    print(c)
     directory = "/home/mlab/cea-os/ceaos/resources/config/recipes"
 
     for recipe in os.scandir(directory):
