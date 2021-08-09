24678c256595e7ba181da6f95106fc0c679f0a92
kxu11235 <xukev@seas.upenn.edu>
2021-08-09 14:52:27 -0400

    Interfaced with image_analysis_sensor

--- ceaos/sensors/stitcher.py
+++ ceaos/sensors/stitcher.py
@@ -8,28 +8,19 @@ from .image_analysis_sensor import *
 
 class Stitcher():
     def __init__(self):
-        self.images = []
-        self.stitched_img = None
-        self.location = None
+        self.stitched = None
         self.output_dir = './images/stitched.png'
-        self.analytics_sensor = None
 
-    def set_location(self, location):
-        self.location = location
-
-    def set_analytics(self, sensor):
-        self.analytics_sensor = sensor
-
-    def stitch_images(self):
+    def stitch_images(self, images):
         stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
-        (status, self.stitched) = stitcher.stitch(self.images)
+        (status, self.stitched) = stitcher.stitch(images)
         if status == 0:
             # write the output stitched image to file
             cv2.imwrite(self.output_dir, self.stitched)
-            if self.analytics_sensor is not None:
-                self.analytics_sensor.mask_images()
+            return self.output_dir
 
         # otherwise the stitching failed, likely due to not enough keypoints)
         # being detected
         else:
             print("[INFO] image stitching failed ({})".format(status))
+            return None
